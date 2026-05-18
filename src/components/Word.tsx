ec370c8ade3be907896a070057a88cc4986289f9
Sami Romdhana <sami.romdhana@gmail.com>
2026-05-18 13:24:41 +0200

    Letter counts

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -1,14 +1,18 @@
 "use client";
 
+import styles from "./Word.module.css";
+
 interface WordProps {
   content: string;
   revealed: boolean;
 }
 
 export function Word(props: WordProps) {
-  if (props.revealed) {
-    return props.content;
-  } else {
-    return "█".repeat(props.content.length);
-  }
+  if (props.revealed) return props.content;
+
+  return (
+    <span className={styles.overlay} data-count={props.content.length}>
+      {"█".repeat(props.content.length)}
+    </span>
+  );
 }
