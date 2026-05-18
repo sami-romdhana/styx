b2c7b9e276b5f6038bf0ec400c1284ced5b61b45
Sami Romdhana <sami.romdhana@gmail.com>
2026-05-18 20:34:07 +0200

    Enhance style of page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,14 @@ import styles from "./Header.module.css";
 export function Header() {
   return (
     <header className={styles.header}>
-      <h1>Styx</h1>
-      <p>Guess the game from its' store description</p>
+      <div>
+        <h1>Styx</h1>
+        <p>Guess the game from its' store description</p>
+
+        <span />
+
+        <a href="/about">Help</a>
+      </div>
     </header>
   );
 }
